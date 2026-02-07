7263bf15fa83c93a13f65ad524dfaee11f8c50ec
Nicolas Gres <nicolas@gres.io>
2026-02-08 00:11:29 +0100

    improve docker image

---
+++ scripts/docker/README.md
@@ -0,0 +1,24 @@
+# Docker Setup
+
+## Train Container
+
+### Build
+
+```shell
+docker build --target training -t openpi_train -f scripts/docker/Dockerfile .
+```
+
+```shell
+docker save openpi_train | gzip | ssh slurm "cat > ~/openpi_train.tar.gz"
+```
+
+### Run
+
+```shell
+docker run -it --rm \
+    --gpus all \
+    -v $(pwd):/app \
+    -e TRAIN_CONFIG=pi05_leros2_aa \
+    -e TRAIN_ARGS="--resume" \
+    openpi_train
+```
--- scripts/docker/train.sh
+++ scripts/docker/train.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Compute norm stats
-uv run scripts/compute_norm_stats.py --config-name $TRAIN_CONFIG
+if [ "$SKIP_STATS" != "true" ]; then
+    uv run /app/scripts/compute_norm_stats.py --config-name $TRAIN_CONFIG || exit 1
+fi
 
 # Train
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.9
-uv run scripts/train.py $TRAIN_CONFIG --exp-name=$EXP_NAME --overwrite
+uv run /app/scripts/train.py $TRAIN_CONFIG $TRAIN_ARGS || exit 1
